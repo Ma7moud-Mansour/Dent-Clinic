7755d3e8f694b6f248280eeffc4c07c12bfe2596
mahmoud <mahmoudabdelkareem.info@gmail.com>
2026-01-29 00:51:14 +0200

    Create DataBase

--- myproject/clinic/admin.py
+++ myproject/clinic/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Patient, Appointment, Visit, Payment
+
+admin.site.register(User)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(Visit)
+admin.site.register(Payment)
