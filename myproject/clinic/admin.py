a9003d1753e24269ccea5652a68560360cbf2307
mahmoud <mahmoudabdelkareem.info@gmail.com>
2026-01-30 16:20:05 +0200

    Fix Password Hashing in Django admin panel

--- myproject/clinic/admin.py
+++ myproject/clinic/admin.py
@@ -1,9 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
 from .models import User, Patient, Appointment, Visit, Payment
 
-admin.site.register(User)
+class ClinicUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'role')
+
+
+class ClinicUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+@admin.register(User)
+class ClinicUserAdmin(UserAdmin):
+    add_form = ClinicUserCreationForm
+    form = ClinicUserChangeForm
+    model = User
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'role', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
 admin.site.register(Patient)
 admin.site.register(Appointment)
 admin.site.register(Visit)
